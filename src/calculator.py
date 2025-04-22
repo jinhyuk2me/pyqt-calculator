d6ba791cf5998015870240569992ec430cbbbf86
장진혁 <164614732+jinhyuk2me@users.noreply.github.com>
2025-04-22 19:41:56 +0900

    add img

--- src/calculator.py
+++ src/calculator.py
@@ -83,7 +83,6 @@ class Calculator(QDialog, form_class):
             else :
                 return
 
-        # 괄호 뒤 음수 처리: ( -3 ) → "-3"
         if self.tokens and self.tokens[-1] == "(" and op == "-":
             self.current_input = "-"
             self.just_calculated = False
@@ -129,8 +128,6 @@ class Calculator(QDialog, form_class):
     def press_equal(self):
         if self.current_input:
             self.tokens.append(self.current_input)
-        #merged_tokens = self.merge_negative_numbers(self.tokens)
-        #postfix = self.to_postfix(merged_tokens)
         postfix = self.to_postfix(self.tokens)
         try:
             if postfix == "Error":
@@ -268,22 +265,6 @@ class Calculator(QDialog, form_class):
         except:
             self.just_errored = True
             return "Error"
-        
-    # ----------------------------------------------------------------------
-
-    # def merge_negative_numbers(self, tokens):
-    #     result = []
-    #     i = 0
-    #     while i < len(tokens):
-    #         if (tokens[i] == "-" and
-    #             i > 0 and tokens[i - 1] == "(" and
-    #             i + 1 < len(tokens) and self.is_number(tokens[i + 1])):
-    #             result.append("-" + tokens[i + 1])
-    #             i += 2
-    #         else:
-    #             result.append(tokens[i])
-    #             i += 1
-    #     return result
     
     # ----------------------------------------------------------------------
 
